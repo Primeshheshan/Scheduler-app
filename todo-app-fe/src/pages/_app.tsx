643a4bcf57d1691a02c5cfde611a8acce586f27f
PrimeshHeshan <primeshheshan@gmail.com>
2023-05-30 17:51:52 +0530

    feat: remove login page from layout

--- todo-app-fe/src/pages/_app.tsx
+++ todo-app-fe/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from '@/components/layout';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <>
+      {router.pathname !== '/login' ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
