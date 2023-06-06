e3fad6902cd9c9bafc09fd2dafa3ef02ccba9291
PrimeshHeshan <primeshheshan@gmail.com>
2023-06-06 18:26:28 +0530

    feat: implement signup page

--- todo-app-fe/src/pages/_app.tsx
+++ todo-app-fe/src/pages/_app.tsx
@@ -2,22 +2,16 @@ import Layout from '@/components/layout';
 import store from '@/redux';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   return (
     <>
-      {router.pathname !== '/login' ? (
-        <Provider store={store}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </Provider>
-      ) : (
-        <Component {...pageProps} />
-      )}
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </>
   );
 }
--- todo-app-fe/src/pages/index.tsx
+++ todo-app-fe/src/pages/index.tsx
@@ -18,6 +18,7 @@ import {
   Button,
   Card,
   Input,
+  Spinner,
   Textarea,
   Typography,
 } from '@material-tailwind/react';
@@ -44,27 +45,26 @@ export default function Home() {
     title: Yup.string().required('Title is required'),
   });
 
-  useEffect(() => {
-    if (!localStorage.getItem('isLoggedIn')) {
-      localStorage.setItem('isLoggedIn', 'true');
-      showAlert(
-        'User created successfully!',
-        'Congratulations, your account has been successfully created. Thank you for being awesome!',
-        'green'
-      );
-    }
-  }, [showAlert]);
+  // useEffect(() => {
+  //   if (!localStorage.getItem('isLoggedIn')) {
+  //     showAlert(
+  //       'User created successfully!',
+  //       'Congratulations, your account has been successfully created. Thank you for being awesome!',
+  //       'green'
+  //     );
+  //   }
+  // }, [showAlert]);
 
-  useEffect(() => {
-    setTodos(todos);
-    if (error) {
-      showAlert(
-        'Task fetching failed!',
-        'Opps something went wrong, please try again!',
-        'red'
-      );
-    }
-  }, [error, showAlert, todos]);
+  // useEffect(() => {
+  //   setTodos(todos);
+  //   if (error) {
+  //     showAlert(
+  //       'Task fetching failed!',
+  //       'Opps something went wrong, please try again!',
+  //       'red'
+  //     );
+  //   }
+  // }, [error, showAlert, todos]);
 
   const formik = useFormik({
     initialValues: {
@@ -136,6 +136,11 @@ export default function Home() {
   return (
     <>
       <div className='md:mx-auto max-w-screen-md py-12 mx-2'>
+        <div className='relative q-full max-q-lg top-56'>
+          <div className='absolute top-0 -left-2 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob'></div>
+          <div className='absolute top-0 right-64 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000'></div>
+          <div className='absolute top-20 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000'></div>
+        </div>
         <Card className='p-5'>
           <Typography variant='h4' color='blue-gray' className='mb-5'>
             New Todo
@@ -191,7 +196,6 @@ export default function Home() {
           No Todos
         </Typography>
       )}
-
       <AlertPopup
         open={openAlert}
         setOpen={setOpenAlert}
